cce449853580ea176b0e2cf5c17ba5a4d5bdada3
Brian Sanabria <brisaning@gmail.com>
2025-04-03 10:45:20 -0500

    ajustes para corregir error en theme

--- saludo_brisan.module.php
+++ saludo_brisan.module.php
@@ -2,21 +2,22 @@
 
 /**
  * @file
- * Contains saludo_brisan.module.
+ * Contains saludo_brisan.module.php.
  */
 
-use Drupal\Core\Render\Element;
+ use Drupal\Core\Render\Element;
 
-/**
- * Implements hook_theme().
- */
-function saludo_brisan_theme() {
-  return [
-    'saludo_brisan' => [
-      'variables' => [
-        'nombre' => NULL,
-      ],
-      'template' => 'saludo-brisan',
-    ],
-  ];
-}
+ /**
+  * Implements hook_theme().
+  */
+ function saludo_brisan_theme($existing, $type, $theme, $path) {
+   return [
+     'saludo_brisan' => [
+       'render element' => 'elements',
+       'variables' => [
+         'nombre' => NULL,
+       ],
+       'template' => 'saludo-brisan',
+       'path' => $path . '/templates',
+     ],
+   ];
--- src/Controller/SaludoController.php
+++ src/Controller/SaludoController.php
@@ -7,12 +7,9 @@ use Drupal\Core\Controller\ControllerBase;
 class SaludoController extends ControllerBase {
 
   public function saludo() {
-    // Obtiene el usuario actual a través del servicio
     $account = \Drupal::currentUser();
-    // Obtener el nombre del usuario o "visitante" si es anónimo
     $nombre = $account->isAnonymous() ? $this->t('visitante') : $account->getDisplayName();
 
-    // Construir el render array con el template
     return [
       '#theme' => 'saludo_brisan',
       '#nombre' => $nombre,
